a4c09317cfbf4871163da8bdb70d7971da32bcfe
angelica17200 <marialopez@colegioguanenta.edu.co>
2023-07-17 18:39:34 -0500

    operadores

---
+++ 3.operadores.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores</title>
+</head>
+<body>
+    <h1>Operadores</h1>
+    <?php
+    //Operadores aritméticos
+    /* Recuerda que un "operador" es un símbolo que le indica a la máquina una operación o acción a realizar.
+     Desde su nombre, los operadores aritméticos indican que se relacionan a las operaciones matemáticas 
+     que usan con frecuencia y son de común conocimiento.  Un operador aritmético trabaja en el código manipulando 
+     los datos numéricos que puedan presentarse. Es así como se evidencian dos tipos: binarios y unitarios.*/
+
+     
+
+    //Operadores de comparación 
+    /* Los operadores de comparación, tal como su nombre lo indica, comparan los valores de una condición y las
+     identifican en dos estados: falsa o verdadera. */
+
+     //Tipos de comparación
+     /*Igualdad: representado con el símbolo (=) en la mayoría de lenguajes.*/
+     /* Desigualdad: es inverso al de igualdad.*/
+     /*Mayor que: compara dos valores y valida que el número de la izquierda sea mayor que el de la derecha.*/
+     /* Menor que: determina si el valor de la izquierda es menor que el de la derecha.*/
+
+    //Operadores lógicos 
+    /* Este tipo de operadores permite obtener solo dos resultados, por lo que se conocen también como booleanos, 
+    porque hacen uso de los principios del álgebra de Boole. Los resultados son Verdadero o Falso. */
+    
+    //Operadores de union de cades
+
+    /*Los operadores de unión se utilizan para especificar el tipo de coincidencia entre columnas que están unidas en una relación.
+
+    De forma predeterminada, los valores se comparan utilizando el operador de igual (=). Cuando utiliza un operador que no es el signo de igual
+    (=), crea uniones basadas en un rango de valores. */
+
+    ?>
+    
+</body>
+</html>
